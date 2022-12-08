2091419e9f64f61e9be67379fd7ff732ffbff827
AnnaNesterenko <nura_arh@ukr.net>
2022-12-08 13:47:27 +0200

    addBG

--- src/pages/DashboardPage/Dashboard.styled.js
+++ src/pages/DashboardPage/Dashboard.styled.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 import { respondTo } from '../../helpers/respondTo'
+// import mediumPinkImg from '../../assets/images/ellipsesBg/EllipsePinkTablet.png'
+// import mediumVioletImg from '../../assets/images/ellipsesBg/EllipseVioletTablet.png'
 
 export const MainContainer = styled.div`
   padding: 12px 20px;
@@ -20,7 +22,9 @@ export const MainContainer = styled.div`
   	flex-direction: row;
   	align-items:flex-start;
   	min-width: 1200px;
-  	padding: 0px 16px;`}
+  	padding: 0px 16px;
+
+	`}
 `
 export const ContainerLeft = styled.div`
   display: flex;
@@ -36,4 +40,27 @@ export const ContainerTop = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+`
+
+export const BG = styled.div`
+  background: rgba(255, 255, 255, 0.4);
+  &::before {
+    ${respondTo.sm`
+    content: '';
+    position: absolute;
+    z-index: -1;
+    right: 0;
+    left: 0;
+
+	height: 100%;
+    background-image: url(${p => p.mediumPinkImg}),
+      url(${p => p.violetImg});
+    background-repeat: no-repeat;
+    background-position: right top, left bottom;
+    filter: blur(25px);`}
+
+    ${respondTo.lg`
+   background-image: url(${p => p.largePinkImg}),
+      url(${p => p.violetImg})`}
+  }
 `
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -5,63 +5,77 @@ import Balance from '../../components/Balance/Balance'
 import DashbordBtns from '../../components/DashbordBtns/DashbordBtns'
 import Header from '../../components/Header/Header'
 import Loader from '../../components/Loader/Loader'
-import { ContainerLeft, ContainerTop, MainContainer } from './Dashboard.styled'
+import {
+  BG,
+  ContainerLeft,
+  ContainerTop,
+  MainContainer
+} from './Dashboard.styled'
 import Media from 'react-media'
+import mediumPinkImg from '../../assets/images/ellipsesBg/EllipsePinkTablet.png'
+import violetImg from '../../assets/images/ellipsesBg/EllipseViolet.png'
+import largePinkImg from '../../assets/images/ellipsesBg/EllipsePink.png'
 
 const DashboardPage = () => {
   return (
     <>
       <Header />
-      <MainContainer>
-        {
-          <Media
-            queries={{
-              small: '(max-width: 767px)',
-              medium: '(min-width: 768px) and (max-width: 1199px)',
-              large: '(min-width: 1200px)'
-            }}
-          >
-            {matches => (
-              <>
-                {matches.small && (
-                  <>
-                    <ContainerLeft>
-                      aaaaa
-                      <DashbordBtns />
-                      <Balance />
-                    </ContainerLeft>
-                  </>
-                )}
-                {matches.medium && (
-                  <>
-                    <ContainerTop>
+      <BG
+        mediumPinkImg={mediumPinkImg}
+        violetImg={violetImg}
+        largePinkImg={largePinkImg}
+      >
+        <MainContainer>
+          {
+            <Media
+              queries={{
+                small: '(max-width: 767px)',
+                medium: '(min-width: 768px) and (max-width: 1199px)',
+                large: '(min-width: 1200px)'
+              }}
+            >
+              {matches => (
+                <>
+                  {matches.small && (
+                    <>
                       <ContainerLeft>
-                        fffff
+                        aaaaa
                         <DashbordBtns />
                         <Balance />
                       </ContainerLeft>
-                      <Currency />
-                    </ContainerTop>
-                  </>
-                )}
-                {matches.large && (
-                  <>
-                    <ContainerLeft>
-                      ddddd
-                      <DashbordBtns />
-                      <Balance />
-                      <Currency />
-                    </ContainerLeft>
-                  </>
-                )}
-              </>
-            )}
-          </Media>
-        }
-        <Suspense fallback={<Loader />}>
-          <Outlet />
-        </Suspense>
-      </MainContainer>
+                    </>
+                  )}
+                  {matches.medium && (
+                    <>
+                      <ContainerTop>
+                        <ContainerLeft>
+                          fffff
+                          <DashbordBtns />
+                          <Balance />
+                        </ContainerLeft>
+                        <Currency />
+                      </ContainerTop>
+                    </>
+                  )}
+                  {matches.large && (
+                    <>
+                      <ContainerLeft>
+                        ddddd
+                        <DashbordBtns />
+                        <Balance />
+                        <Currency />
+                      </ContainerLeft>
+                    </>
+                  )}
+                </>
+              )}
+            </Media>
+          }
+          <Suspense fallback={<Loader />}>
+            <Outlet />
+          </Suspense>
+        </MainContainer>
+      </BG>
     </>
   )
 }
