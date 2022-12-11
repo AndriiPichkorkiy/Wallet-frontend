985a367c9a0ca1e278f9a0b2f2f1c0c22630a829
Andrey <93935023+desertptu4ka@users.noreply.github.com>
2022-12-11 15:10:52 +0200

    resolve again resolved conflict with transaction api

--- src/services/transactionsApi.js
+++ src/services/transactionsApi.js
@@ -1,15 +1,11 @@
-
-import { createApi ,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-import { axiosBaseQuery } from '../helpers/axiosBaseQuery'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOTIwZjUwODM1NGQ0MjI4YWJhZTFkYSIsImlhdCI6MTY3MDUxNjczMCwiZXhwIjoxNjcwNzc1OTMwfQ.rm5CXJZqBA9-Wq1-CzcPW8bsWWNzva8zllPNbi4EhbE'
-
 const BASE_URL = 'https://wallet-project-m5us.onrender.com'
 
 export const transactionsApi = createApi({
   reducerPath: 'transactions',
   tagTypes: ['Transactions'],
-
-   baseQuery: fetchBaseQuery({
+  baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
     prepareHeaders: (headers, { getState }) => {
       const token = getState().token
@@ -20,38 +16,28 @@ export const transactionsApi = createApi({
     }
   }),
 
-    prepareHeaders: (headers, { getState }) => {
-      const token = getState().token
-      console.log('token', token)
-      if (token) {
-        headers.set('authorization', `Bearer ${token}`)
-      }
-      return headers
-    }
-  }),
   endpoints: builder => ({
     getAllTransactions: builder.query({
       query: () => ({ url: '/api/transactions/getAll', method: 'GET' }),
-
-      invalidatesTags: [{ type: 'Transactions' }],
-
-      
+      invalidatesTags: [{ type: 'Transactions' }]
     }),
     addTransactions: builder.mutation({
-        query: (initialTransaction) => ({ url: '/api/transactions', method: 'POST' ,
-          body: initialTransaction
-          
-        }),
-        invalidatesTags: [{ type: 'Transactions' }],
+      query: initialTransaction => ({
+        url: '/api/transactions',
+        method: 'POST',
+        body: initialTransaction
+      }),
+      invalidatesTags: [{ type: 'Transactions' }]
     }),
-    getCategory:builder.query({
+    getCategory: builder.query({
       query: () => ({ url: '/api/transactions/categories', method: 'GET' }),
-      invalidatesTags: [{ type: 'Transactions' }],
-
-      
-    }),
-
+      invalidatesTags: [{ type: 'Transactions' }]
+    })
   })
 })
 
-export const { useGetAllTransactionsQuery ,useAddTransactionsMutation,useGetCategoryQuery} = transactionsApi
+export const {
+  useGetAllTransactionsQuery,
+  useAddTransactionsMutation,
+  useGetCategoryQuery
+} = transactionsApi
