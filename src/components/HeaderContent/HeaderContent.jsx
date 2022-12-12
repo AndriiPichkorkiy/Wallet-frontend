1fb8011a53e80ff21a9bb509947fdbcdb281646e
Nikita P <nikita.panasevich22@gmail.com>
2022-12-12 21:20:15 +0200

    added icon with letter from name, but comented

--- src/components/HeaderContent/HeaderContent.jsx
+++ src/components/HeaderContent/HeaderContent.jsx
@@ -8,34 +8,34 @@ import {
   BtnLogout,
   LogoutCont,
   LogoutExitName,
-  LogoutExit
+  LogoutExit,
+  // LogoutExitNameCircle
 } from './HeaderComponents'
 import LogoutIcon from '@mui/icons-material/Logout'
 import { useSelector } from 'react-redux'
-import { NavLink } from 'react-router-dom'
 
 export const HeaderContent = ({ toExit }) => {
-  // const name = useSelector(store => store.user.user.name)
-  const email = useSelector(store => store.user.user.email)
-  const name = email.split("@")[0]
-  console.log(name)
+  const nameAll = useSelector(store => store.user.user.name)
+  // const name = nameAll.split('')[0].toUpperCase()
   return (
     <>
       <HeaderC>
         <LogoCont>
-          {/* <NavLink to="/"> */}
             <Logo src={wallet} alt='logo' />
             <LogoText>Wallet</LogoText>
-          {/* </NavLink> */}
         </LogoCont>
         <LogoutCont>
-          <LogoutExitName className={'name'}>{name}</LogoutExitName>
+          {/* <LogoutExitNameCircle className={'name'}>
+            {name}
+          </LogoutExitNameCircle> */}
+          <LogoutExitName>
+            {nameAll}
+          </LogoutExitName>
           <BtnLogout onClick={toExit} type='btn'>
             <LogoutIcon
               sx={{
                 color: '#BDBDBD'
               }}
-              // src={logout} alt="logout"
             />
             <LogoutExit>Exit</LogoutExit>
           </BtnLogout>
